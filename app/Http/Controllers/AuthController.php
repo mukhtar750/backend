96346195589735a34c5afca5c4a2dc81016d5196
Shittu Muhammed Mukhtar <shittumuhammaddmukhtar@gmail.com>
2025-06-25 19:38:41 +0100

    api added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Login successful']);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aryaas.netlify.app'],
+    'allowed_origins' => ['https://aryaas.netlify.app'], // <- Frontend domain
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
