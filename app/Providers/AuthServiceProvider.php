1bd9afdb27bf4cd9568bb55a5a20d699778c2dd5
Shittu Muhammed Mukhtar <shittumuhammaddmukhtar@gmail.com>
2025-08-29 15:20:11 +0100

    Fix 403 error: Register ResourcePolicy in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\TrainingModule;
+use App\Models\Resource;
 use App\Policies\TrainingModulePolicy;
+use App\Policies\ResourcePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         TrainingModule::class => TrainingModulePolicy::class,
+        Resource::class => ResourcePolicy::class,
     ];
 
     /**
